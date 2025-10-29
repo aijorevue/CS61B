4a900eafc47271577af5e8b6566bfe83d7f95179
aijorevue <qinnovation013023@163.com>
2025-10-29 09:37:10 +0800

    提交 OffByN 实现

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+public class OffByN implements CharacterComparator{
+    public int coutn_minus;
+    public OffByN(int N)
+    {
+        coutn_minus=N;
+    }
 
-public class OffByN {
+    @Override
+    public boolean equalChars(char x, char y) {
+        if(Math.abs(x-y)==coutn_minus)
+        {
+            return true;
+        }
+        return false;
+    }
 }
