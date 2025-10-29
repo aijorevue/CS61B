5c2068e559455fe2d4d046a8d63b2543c6a2b2b2
aijorevue <qinnovation013023@163.com>
2025-10-29 09:37:10 +0800

    提交 OffByOne 实现

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+public class OffByOne implements CharacterComparator{
 
-public class OffByOne {
+    public int abs(int x,int y)
+    {
+        if(x-y<0)
+        {
+            return -(x-y);
+        }
+        else
+        {
+            return x-y;
+        }
+    }
+    @Override
+    public boolean equalChars(char x,char y)
+    {
+        if(abs((int) x,(int)y)==1)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
