b9db1a0ee887407c46b4ac271c4fe5d786fcd360
aijorevue <qinnovation013023@163.com>
2025-10-29 09:37:08 +0800

    提交 Palindrome 实现

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
-
 public class Palindrome {
+
+    public Deque<Character> wordToDeque(String word) {
+        Deque<Character> str=new LinkedListDeque<>();
+        for(int i=0;i<word.length();i++)
+        {
+            str.addLast(word.charAt(i));
+        }
+        return str;
+    }
+    public boolean isPalindrome(String c){
+        if(c.length()<=1)
+        {
+            return true;
+        }
+        int a=c.length();
+        for(int i=0;i<a/2;i++)
+        {
+            if(c.charAt(i)==c.charAt(a-1-i))
+            {
+                continue;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean isPalindrome(String word,CharacterComparator<Character> cc)
+    {
+        if(word.length()<=1) {
+            return true;
+        }
+        for(int i=0;i<word.length()/2;i++)
+        {
+
+            if(!cc.equalChars(word.charAt(i),word.charAt(word.length()-i-1)))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 }
